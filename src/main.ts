820ad50b29e74bc1a562c20a8f0a5f4e13929067
isuraem <erandaim@gmail.com>
2025-01-09 19:51:47 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Set global prefix for all routes
-  app.setGlobalPrefix('api');
-
   app.enableCors();
   await app.listen(8000);
-  console.log('Nest application successfully started on port 8000 with routes prefixed by /api');
+  console.log('Nest application successfully started on port 8000');
+
 }
 bootstrap();
